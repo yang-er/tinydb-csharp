d1fc26ff72a77e2c9db0895a11dea1af33a4f667
小羊 <tlylz99@live.cn>
2019-06-12 12:00:23 +0800

    添加linq形式的查询实例

--- TinyDB/Program.cs
+++ TinyDB/Program.cs
@@ -9,6 +9,9 @@ namespace TinyDb
         {
             var people = new DbSet<Person>("person");
             foreach (var item in people) Console.WriteLine(item.ToString());
+            var query = (from p in people
+                         where p.Id >= 111 && p.Id < 250 && p.Name.StartsWith("l")
+                         select new { p.Id, p.Name }).ToList();
             var list = people
                 .Where(p => (p.Id >= 111) && p.Id < 250 && p.Name.StartsWith("l"))
                 .Select(p => new { p.Id, p.Name })
